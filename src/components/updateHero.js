7883c98d98c9f68284f73220f3ef61c472cba77c
uguumur <uguumur.oldokh@gmail.com>
2023-04-11 18:25:18 +0800

    new task

---
+++ src/components/updateHero.js
@@ -0,0 +1,5 @@
+export default function UpdateHero() {
+  // declare a state named hero => { name, img, power, health, speed }
+  // coin => 10000
+  return <div></div>;
+}
---
+++ src/pages/update.js
@@ -0,0 +1,6 @@
+export default function Update() {
+  return (
+    // update Hero component дуудах
+    <div>U</div>
+  );
+}
